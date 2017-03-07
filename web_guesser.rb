8a86432784fc1dd7cf47aa92d710be05bf2578ac
glassjoseph <glassjoseph@gmail.com>
2017-03-07 07:41:19 -0700

    corrected color shades

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,26 +1,31 @@
 require "sinatra"
 require "sinatra/reloader"
 
-
-secret_number = rand(99) + 1  
+secret_number = rand(99) + 1
+@background_color = "Cornflower Blue"
 
 get '/' do
   guess = params["guess"].to_i
   message = check_guess(guess, secret_number)
-  erb :index, :locals => {:secret_number => secret_number, :message => message}
+  erb :index, :locals => {:secret_number => secret_number, :message => message, :background_color => @background_color}
 end
 
 
 def check_guess(guess, secret_number)
   if guess == secret_number
+    @background_color = "Green"
     message = "Correct! \nThe SECRET NUMBER is #{secret_number}."
   elsif guess > (secret_number + 5)
+    @background_color = "Red"
     message = "Way too high!"
   elsif guess < (secret_number - 5)
+    @background_color = "Red"
     message = "Way too low!"
   elsif guess > secret_number
+    @background_color = "#ffb3b3"
     message = "Too high!"
   elsif guess < secret_number
+    @background_color = "#ffb3b3"
     message = "Too low!"
   end
 end
